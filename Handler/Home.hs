f117d0e0a8d225a33a512c04ed6847be05e2e1ad
nek0 <nek0@chelnok.de>
2015-02-20 03:40:24 +0100

    now bisecting the IP nicely

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,6 +4,7 @@ import Import
 import Network.Wai
 import Data.Maybe
 import qualified Data.Text as T
+import qualified Data.List as L
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -14,8 +15,21 @@ import qualified Data.Text as T
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-  ip <- (fst . (T.breakOn ":") . T.pack . show . remoteHost . reqWaiRequest) <$> getRequest
+  rawIp <- (show . remoteHost . reqWaiRequest) <$> getRequest
+  ip <- case '[' `L.elem` rawIp of
+    True -> return $ extractIpv6 rawIp
+    False -> return $ extractIpv4 rawIp
   user <- lookupGetParam "user"
   passwd <- lookupGetParam "passwd"
   defaultLayout $ do
     $( widgetFile "home")
+
+extractIpv6 :: String -> String
+extractIpv6 rawIp =
+  case '.' `elem` rawIp of
+    False -> L.take (1 + (fromJust $ L.findIndex ( == ']') rawIp)) rawIp
+    True -> drop 8 (fst (L.splitAt (fromJust $ L.findIndex ( == ']') rawIp) rawIp))
+
+extractIpv4 :: String -> String
+extractIpv4 rawIp =
+  fst (L.splitAt (fromJust $ L.findIndex ( == ':') rawIp) rawIp)
